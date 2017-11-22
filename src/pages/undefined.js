ad0eff11dbb0d2b3a306ae989daccd32c1b7ddd9
Harrison <hmcguire13@gmail.com>
2017-11-22 07:18:07 -0500

    added undefined page

---
+++ src/pages/undefined.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const UndefinedPage = () => (
+  <div>
+    <h1>Undefined Page</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </div>
+)
+
+export default UndefinedPage
