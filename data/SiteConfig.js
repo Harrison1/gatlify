6dff319a49c24426c0535439c8bc8e13e8f21824
Harrison <hmcguire13@gmail.com>
2017-11-24 14:41:35 -0500

    site config update

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,13 +3,13 @@ module.exports = {
     siteTitle: "Gatsby Netlify CMS Starter", // Site title.
     siteTitleAlt: "GatsbyJS Netlify CMS Starter", // Alternative site title for SEO.
     siteLogo: "https://api.adorable.io/avatars/150/logo.png", // Logo used for SEO and manifest.
-    siteUrl: "https://my-super-net-site.netlify.com/", // Domain of your website without pathPrefix.
+    siteUrl: "https://gatlify.netlify.com/", // Domain of your website without pathPrefix.
     pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
     siteDescription: "A GatsbyJS stater with Netlify CMS Integration", // Website description used for RSS feeds/meta description tag.
     siteRss: "/rss.xml", // Path to the RSS file.
     siteFBAppID: "1825356251115265", // FB Application ID for using app insights
     googleAnalyticsID: "UA-47311644-5", // GA tracking ID.
-    userName: "User Name", // Username to display in the author segment.
+    userName: "Harrison McGuire", // Username to display in the author segment.
     copyright: "Copyright © 2017. Gatsby Netlify CMS Starter", // Copyright string for the footer of the website and RSS feed.
     themeColor: "#663399", // Used for setting manifest and progress theme colors.
     backgroundColor: "#0086b3" // Used for setting manifest background color.
